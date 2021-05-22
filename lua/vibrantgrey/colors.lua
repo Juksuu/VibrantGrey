fb43ed8c7c503641a18266a88a10accb6012001c
Frans Paasonen <franspaasonen@gmail.com>
2021-05-22 20:29:49 +0300

    feat: Start using colorbuddy, add lua highlighting

--- lua/vibrantgrey/colors.lua
+++ lua/vibrantgrey/colors.lua
@@ -1,41 +1,24 @@
-local utils = require("vibrantgrey.utils")
+-- local utils = require('vibrantgrey.utils')
 
-local colors = {
-    none = "NONE",
-    black = "#000000",
-    white = "#ffffff",
+local Color = require("colorbuddy").setup()
 
-    bg = "#3a383f",
-    fg = "#d0d6d2",
-    fg2 = "#5c6773",
+local M = {}
 
-    green = "#53ed75",
-    red = "#dd3033",
-    blue = "#537eed",
-    yellow = "#cbed53",
+function M.createColors()
+  Color.new('white', '#ffffff')
+  Color.new('black', '#000000')
+  Color.new('green', '#53ed75')
+  Color.new('red', '#dd3033')
+  Color.new('blue', '#537eed')
+  Color.new('yellow', '#cbed53')
+  Color.new('pink', '#ed537e')
+  Color.new('orange', '#cc9f1a')
+  Color.new('orange2', '#ed7553')
 
-    pink = "#ed537e",
-    orange = "#cc9f1a",
-    orange2 = "#ed7553"
-  }
 
-  utils.bg = colors.bg
+  Color.new('default_bg', '#3a383f')
+  Color.new('default_fg', '#d0d6d2')
+  Color.new('fg_highlight', '#5c6773')
+end
 
-  colors.error = colors.red
-  colors.warning = colors.yellow
-  colors.info = colors.blue
-  colors.hint = colors.green
-  colors.string = colors.green
-
-  colors.statement = utils.adjust_brightness(colors.blue, 2)
-  colors.bg_highlight = utils.adjust_brightness(colors.bg, 1.5)
-  colors.bg_highlight2 = utils.adjust_brightness(colors.fg2, 0.8)
-
-  colors.diff = {
-    add = utils.adjust_brightness(colors.green, 0.7),
-    delete = utils.adjust_brightness(colors.red, 0.7),
-    change = utils.adjust_brightness(colors.blue, 0.7),
-    text = colors.blue
-  }
-
-return colors
+return M
---
+++ lua/vibrantgrey/groups.lua
@@ -0,0 +1,122 @@
+local Color, colors, Group, groups, styles = require("colorbuddy").setup()
+
+local M = {}
+
+function M.createGroups()
+  local bg_highlight = colors.fg_highlight:dark(0.2)
+
+  -- Base groups
+
+  Group.new('Normal', colors.default_fg, colors.default_bg) -- Normal text
+  Group.new('Cursor', colors.default_bg, colors.default_fg) -- Character under cursor
+  Group.new('Directory', colors.green) -- Directory names
+  Group.new('VertSplit', bg_highlight, bg_highlight)
+  Group.new('LineNr', colors.fg_highlight)
+  Group.new('CursorLineNr', colors.white)
+  Group.new('Visual', colors.none, bg_highlight)
+  Group.new('Title', colors.pink)
+
+  Group.new('DiffAdd', colors.none, colors.green:dark(0.3))
+  Group.new('DiffChange', colors.none, colors.blue:dark(0.3))
+  Group.new('DiffDelete', colors.none, colors.red:dark(0.3))
+  Group.new('DiffText', colors.none, colors.blue)
+
+  Group.new('SignColumn', colors.fg_highlight)
+  Group.new('SignColumnSB', colors.fg_highlight)
+
+  Group.new('Pmenu', colors.fg, colors.bg:dark(0.1))
+
+  Group.new('Comment', colors.fg_highlight)
+
+  Group.new('Constant', colors.pink)
+  Group.new('Number', colors.pink)
+  -- Group.new('String')
+  -- Group.new('Character')
+  -- Group.new('Boolean')
+  -- Group.new('Float')
+
+  Group.new('Identifier', colors.orange2)
+  Group.new('Function', colors.yellow)
+  Group.new('Operator', colors.orange)
+
+  Group.new('Keyword', colors.pink)
+  -- Group.new('Exception')
+
+  Group.new('Statement', groups.keyword)
+  Group.new('Conditional', colors.blue:light(0.1))
+  Group.new('Repeat', colors.blue:light(0.1))
+  Group.new('Label', colors.blue:light(0.1))
+
+  Group.new('PreProc', colors.pink)
+  -- Group.new('Include')
+  -- Group.new('Define')
+  -- Group.new('Macro')
+  -- Group.new('PreCondit')
+
+  Group.new('Type', groups.keyword)
+  Group.new('Typedef', colors.blue)
+  Group.new('Structure', colors.fg)
+  -- Group.new('StorageClass')
+
+  Group.new('Special', colors.white)
+  Group.new('SpecialChar', colors.white)
+  -- Group.new('Tag')
+  -- Group.new('Delimiter')
+  -- Group.new('SpecialComment')
+  -- Group.new('Debug')
+
+  -- Neovim
+  Group.new('healthError', colors.red)
+  Group.new('healthSuccess', colors.green)
+  Group.new('healthWarning', colors.yellow)
+
+  -- Built-in Lsp
+  Group.new('LspReferenceText', colors.fg_highlight)
+  Group.new('LspReferenceRead', colors.fg_highlight)
+  Group.new('LspReferenceWrite', colors.fg_highlight)
+
+  Group.new('LspDiagnosticsDefaultError', colors.red)
+  Group.new('LspDiagnosticsDefaultWarning', colors.orange)
+  Group.new('LspDiagnosticsDefaultInformation', colors.green)
+  Group.new('LspDiagnosticsDefaultHint', colors.blue)
+
+  Group.new('LspDiagnosticsVirtualTextError', colors.red)
+  Group.new('LspDiagnosticsVirtualTextWarning', colors.orange)
+  Group.new('LspDiagnosticsVirtualTextInformation', colors.blue)
+  Group.new('LspDiagnosticsVirtualTextHint', colors.green)
+
+
+  -- Language Support
+
+  -- Lua
+  Group.new('luaFunc', groups.Function)
+  Group.new('luaStatement', groups.Statement)
+  Group.new('luaKeyword', groups.Keyword)
+  Group.new('luaMyKeyword', groups.Keyword)
+  Group.new('luaFunctionCall', groups.Function)
+  Group.new('luaFuncCall', groups.Function)
+  Group.new('luaFuncKeyword', groups.Keyword)
+  Group.new('luaFuncId', colors.none)
+  Group.new('luaFuncName', groups.Function)
+
+  -- Plugins support
+
+  -- GitSigns
+  Group.new('GitSignsAdd', colors.green:dark(0.3))
+  Group.new('GitSignsChange', colors.blue:dark(0.3))
+  Group.new('GitSignsDelete', colors.red:dark(0.3))
+
+  -- GitGutter
+  Group.new('GitGutterAdd', colors.green:dark(0.3))
+  Group.new('GitGutterChange', colors.blue:dark(0.3))
+  Group.new('GitGutterDelete', colors.red:dark(0.3))
+
+  -- LspSaga
+  Group.new('DiagnosticError', colors.red)
+  Group.new('DiagnosticWarning', colors.orange)
+  Group.new('DiagnosticInformation', colors.blue)
+  Group.new('DiagnosticHint', colors.green)
+
+end
+
+return M
